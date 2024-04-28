947a2e7ef1323e27fea880ca6d1f515393c87977
kshivam49 <kshivam49@gmail.com>
2024-04-28 22:14:07 +0530

    Episode 1

---
+++ Ep-1/App.js
@@ -0,0 +1,13 @@
+// const heading = React.createElement("h1", { id: 'heading' }, "Hello world from React !");
+
+const parent = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" },
+        [React.createElement("h1", {}, "i am h1 tag!"), React.createElement("h3", {}, "h3 tag siblings of h1")]));
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
