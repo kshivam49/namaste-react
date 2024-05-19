ad2065f282ae5299b32498400c036b3d244b2721
kshivam49 <kshivam49@gmail.com>
2024-05-19 18:46:54 +0530

    episode-6

---
+++ Ep-6/src/components/Body.js
@@ -0,0 +1,58 @@
+import RestaurantCard from "./RestaurantCard";
+import { useEffect, useState } from 'react'
+import Shimmer from "./Shimmer";
+
+
+const Body = () => {
+    const [listOfRestaurant, setListOfRestaurant] = useState([]);
+    const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+    const [searchText, setSearchText] = useState('');
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch("https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        // console.log(json);
+        setListOfRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+
+    function filterRestaurant() {
+        setFilteredRestaurant(listOfRestaurant.filter((items) => items.info.avgRating > 4.5));
+    };
+
+
+    if (listOfRestaurant.length == 0) {
+        return <Shimmer />
+    }
+    return (
+        <div className="body">
+            <div className="filters">
+                <div className="search">
+                    <input type="text" className="search-box" value={searchText} onChange={(e) => {
+                        setSearchText(e.target.value);
+                    }} />
+                    <button onClick={() => {
+
+
+                        const filteredList = listOfRestaurant.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase()));
+
+                        setFilteredRestaurant(filteredList);
+                    }}> Search</button>
+                </div>
+
+
+                <button className="filter-btn" onClick={filterRestaurant} >Top Rated Restaurant</button>
+            </div>
+            <div className="res-container">
+                {filteredRestaurant.map((restaurant) => (<RestaurantCard key={restaurant.info.id} resData={restaurant} />))}
+                {/* <RestaurantCard resData={resList[0]} /> */}
+            </div>
+        </div>
+    );
+};
+
+export default Body;
---
+++ Ep-6/src/components/RestaurantCard.js
@@ -0,0 +1,15 @@
+import { CDN_URL } from "../utils/constants";
+
+const RestaurantCard = ({ resData }) => {
+    const { name, cloudinaryImageId, cuisines, avgRating, costForTwo, sla } = resData?.info;
+    return (<div className="res-card" style={{ backgroundColor: "#f0f0f0" }} >
+        <img className="res-logo" src={CDN_URL + cloudinaryImageId} alt="companyThumbnail" />
+        <h3>{name}</h3>
+        <h4>{cuisines.join(', ')}</h4>
+        <h4>{avgRating} stars</h4>
+        <h4>{costForTwo}</h4>
+        <h4>{sla.deliveryTime} minutes</h4>
+    </div>);
+};
+
+export default RestaurantCard;
